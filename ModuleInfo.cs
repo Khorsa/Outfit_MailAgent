c60bc1a7abd17c89769236f4e3d6ee03dc0f4ed1
Roman Stolyarov <rshome@mail.ru>
2025-06-11 23:55:30 +0300

    pre-alpha

--- ModuleInfo.cs
+++ ModuleInfo.cs
@@ -1,20 +1,17 @@
 ﻿using OutfitTool.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MailAgent
 {
     internal class ModuleInfo : ModuleInfoInterface
     {
-        public string Name => "MailAgent";
-
+        public string Name => "mail_agent";
+        public string DisplayName => "MailAgent";
+        public string AssemblyName => "MailAgent";
         public string Description => "Mail Agent";
-
+        public ModuleVersion Version => new ModuleVersion(0, 1, "pre-alpha");
+        public ModuleVersion Require => new ModuleVersion(3, 0);
+        public string Changes => "Первая версия";
         public string Author => "Stolyarov Roman";
-
         public string AuthorContacts => "rshome@mail.ru";
     }
 }
